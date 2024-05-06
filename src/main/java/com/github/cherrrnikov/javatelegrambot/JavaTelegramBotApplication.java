dbc1f1efab55ad93fd959963f89af05613b4d1b2
chera <hiiamdiv@yandex.ru>
2024-05-06 22:51:38 +0300

    Created skeleton for project

---
+++ src/main/java/com/github/cherrrnikov/javatelegrambot/JavaTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.cherrrnikov.javatelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTelegramBotApplication.class, args);
+	}
+
+}
